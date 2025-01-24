56215a66170ef3f06110222de3d17c1790a28a29
philippe.stepniewski <philippe.stepniewski@gmail.com>
2025-01-24 15:05:12 +0100

    added query in ci to test docker image

--- model_platform/__main__.py
+++ model_platform/__main__.py
@@ -6,4 +6,4 @@ This module starts the FastAPI application using Uvicorn.
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("model_platform.api.app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("model_platform.api.app:app", host="0.0.0.0", port=8001, reload=True)
