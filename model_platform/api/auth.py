c8bf11a1185988c9a1121deab6ec892832f982a1
lissachen <chen.lissaelisecharlotte@gmail.com>
2025-02-26 09:00:00 +0100

    fix timezone utc

--- model_platform/api/auth.py
+++ model_platform/api/auth.py
@@ -1,5 +1,5 @@
 import os
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from typing import Optional
 
 from fastapi import Depends, HTTPException
@@ -66,7 +66,7 @@ def authenticate_user(username: str, password: str):
 # Création du token JWT
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
-    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=15))
+    expire = datetime.now(timezone.utc) + (expires_delta or timedelta(minutes=15))
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
