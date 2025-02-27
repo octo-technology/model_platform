430def4455947c06b1d4f56290878b586a0cd6fa
lissachen <chen.lissaelisecharlotte@gmail.com>
2025-02-27 17:50:26 +0100

    stockage du jwt en cookie

--- model_platform/api/auth_routes.py
+++ model_platform/api/auth_routes.py
@@ -1,12 +1,23 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Response
 from fastapi.security import OAuth2PasswordRequestForm
 
 from model_platform.domain.use_cases.auth_usecases import get_current_user, login_for_access_token
 router = APIRouter()
 
 @router.post("/token")
-async def login(form_data: OAuth2PasswordRequestForm = Depends()):
-    return await login_for_access_token(form_data)
+async def login(response: Response, form_data: OAuth2PasswordRequestForm = Depends()):
+
+    token_info_dict = await login_for_access_token(form_data)
+
+    response.set_cookie(
+        key="access_token",
+        value=f"Bearer {token_info_dict .get("access_token")}",
+        httponly=True,
+        samesite="Strict",  # Protection CSRF
+        secure=False  # si True : Indique au navigateur d'envoyer le cookie uniquement sur des connexions HTTPS sécurisées.
+    )
+
+    return {"status": "Authentificated"}
 
 
 @router.get("/me")
