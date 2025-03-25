e530514bed018d8b6286188cd1deb8e4512a9790
Emmanuel-Lin Toulemonde <toul@octo.com>
2025-03-25 15:49:26 +0100

    Remove dead code

--- frontend/project_page.py
+++ frontend/project_page.py
@@ -2,10 +2,8 @@ import streamlit as st
 
 from frontend.api_interactions.deployed_models import get_deployed_models_list
 from frontend.api_interactions.endpoints import DEPLOYED_MODELS_LIST_ENDPOINT, MODELS_LIST_ENDPOINT
-from frontend.api_interactions.governance import get_governance_per_model
 from frontend.api_interactions.models import get_models_list
 from frontend.api_interactions.projects import get_projects_list
-from frontend.st_creators.model_versions_tab import model_versions_tab
 from frontend.st_creators.project_page.project_model_listing import build_model_version_listing
 
 if "tabs" not in st.session_state:
@@ -24,13 +22,3 @@ with tabs[0]:
         build_model_version_listing(models, elements_to_add=["List versions", "Deploy latest"])
         st.write("#### Deployed models")
         build_model_version_listing(deployed_models, elements_to_add=["Undeploy"])
-
-if "list_versions" in st.session_state:
-    with tabs[-1]:
-        model_versions_tab()
-
-with tabs[1]:
-    if "selected_project" in st.session_state:
-        model_gov = get_governance_per_model(st.session_state["selected_project"])
-        for model in model_gov:
-            st.table(model)
