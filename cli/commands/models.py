40a9c464ec0082cc7dcc29e54688f447a2b90a0e
philippe.stepniewski <philippe.stepniewski@octo.com>
2025-09-19 18:03:28 +0200

    add list deployed models

--- cli/commands/models.py
+++ cli/commands/models.py
@@ -1,3 +1,5 @@
+import typer
+
 from cli.utils.api_calls import get_and_print
 
 
@@ -5,9 +7,16 @@ def list_models(project_name: str):
     """List existing models for a project"""
     get_and_print(f"/{project_name}/models/list", "❌ Error retrieving models", success_message="✅ Models retrieved successfully")
 
-def deploy_model(project_name: str, model_name: str, model_version: str):
+def deploy_model(project_name: str, model_name: str = typer.Option(), model_version: str= typer.Option()):
     """Deploy a new model to a project"""
     get_and_print(f"/{project_name}/models/deploy/{model_name}/{model_version}",
                   "❌ Error deploying model", success_message="✅ Model deployed successfully")
 
-#/test/models/deploy/test_model/1
+def undeploy_model(project_name: str, model_name: str = typer.Option(), model_version: str= typer.Option()):
+    """Undeploy a model from a project"""
+    get_and_print(f"/{project_name}/models/undeploy/{model_name}/{model_version}",
+                  "❌ Error undeploying model", success_message="✅ Model undeployed successfully")
+
+def list_deployed_models(project_name: str):
+    """List deployed models for a project"""
+    get_and_print(f"/{project_name}/deployed_models/list", "❌ Error retrieving deployed models", success_message="✅ Deployed models retrieved successfully")
--- cli/main.py
+++ cli/main.py
@@ -1,7 +1,7 @@
 import typer
 
 from cli.commands.auth import login, me
-from cli.commands.models import list_models, deploy_model
+from cli.commands.models import list_models, deploy_model, undeploy_model, list_deployed_models
 from cli.commands.projects import list_projects, add_project, delete_project, project_info, add_user_to_project
 from cli.commands.users import list_users, add_user
 
@@ -20,6 +20,8 @@ project_app.command("add")(add_project)
 project_app.command("add-user")(add_user_to_project)
 project_app.command("list-models")(list_models)
 project_app.command("deploy")(deploy_model)
+project_app.command("undeploy")(undeploy_model)
+project_app.command("list-deployed-models")(list_deployed_models)
 project_app.command("delete")(delete_project)
 user_app.command("list")(list_users)
 user_app.command("add")(add_user)
